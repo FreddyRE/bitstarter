f9d348b8b2ced97a2417ba2d00b60cc3106f26a3
Freddy Romero <freddy_romero@hotmail.com>
2013-07-12 21:31:13 +0000

    Buffer

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World');
+   
+   var buf= new Buffer(256);
+    buf = fs.readFileSync("index.html");
+    response.send(buf.toString('utf8'));
+
+
 });
 
 var port = process.env.PORT || 5000;
